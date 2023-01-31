2e307355905dbc7f6f9ea213e6a4b5d59ddfa5f2
Joao Gosmani <joaogosmani@gmail.com>
2023-01-31 15:47:02 -0300

    Utilizando a interface SimpleRepresentationModelAssembler para centralizar a adição dos links de Cargo

--- src/main/java/br/com/joaogosmani/jgprojetos/api/controllers/CargoControllerApi.java
+++ src/main/java/br/com/joaogosmani/jgprojetos/api/controllers/CargoControllerApi.java
@@ -1,14 +1,12 @@
 package br.com.joaogosmani.jgprojetos.api.controllers;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Link;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,6 +20,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.joaogosmani.jgprojetos.api.dto.CargoDTO;
+import br.com.joaogosmani.jgprojetos.api.hateoas.CargoAssembler;
 import br.com.joaogosmani.jgprojetos.models.Cargo;
 import br.com.joaogosmani.jgprojetos.services.CargoService;
 
@@ -32,45 +31,36 @@ public class CargoControllerApi {
     @Autowired
     private CargoService cargoService;
 
+    @Autowired
+    private CargoAssembler cargoAssembler;
+
     @GetMapping
-    public List<Cargo> buscarTodos() {
+    public CollectionModel<EntityModel<Cargo>> buscarTodos() {
         List<Cargo> cargos = cargoService.buscarTodos();
 
-        cargos.forEach(cargo -> {
-            Long id = cargo.getId();
-
-            Link selfLink = linkTo(methodOn(CargoControllerApi.class).buscarPorId(id))
-                .withSelfRel()
-                .withType("GET");
-
-            Link editarLink = linkTo(methodOn(CargoControllerApi.class).atualizar(null, id))
-                .withSelfRel()
-                .withType("PUT");
-
-                Link excluirLink = linkTo(methodOn(CargoControllerApi.class).excluirPorId(id))
-                    .withSelfRel()
-                    .withType("DELETE");
-
-            cargo.add(selfLink, editarLink, excluirLink);
-        });
-
-        return cargos;
+        return cargoAssembler.toCollectionModel(cargos);
     }
 
     @GetMapping("/{id}")
-    public Cargo buscarPorId(@PathVariable Long id) {
-        return cargoService.buscarPorId(id);
+    public EntityModel<Cargo> buscarPorId(@PathVariable Long id) {
+        Cargo cargo = cargoService.buscarPorId(id);
+
+        return cargoAssembler.toModel(cargo);
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Cargo cadastrar(@RequestBody @Valid CargoDTO cargoDTO) {
-        return cargoService.cadastrar(cargoDTO);
+    public EntityModel<Cargo> cadastrar(@RequestBody @Valid CargoDTO cargoDTO) {
+        Cargo cargo = cargoService.cadastrar(cargoDTO);
+
+        return cargoAssembler.toModel(cargo);
     }
 
     @PutMapping("/{id}")
-    public Cargo atualizar(@RequestBody @Valid CargoDTO cargoDTO, @PathVariable Long id) {
-        return cargoService.atualizar(cargoDTO, id);
+    public EntityModel<Cargo> atualizar(@RequestBody @Valid CargoDTO cargoDTO, @PathVariable Long id) {
+        Cargo cargo = cargoService.atualizar(cargoDTO, id);
+
+        return cargoAssembler.toModel(cargo);
     }
 
 @DeleteMapping("/{id}")
---
+++ src/main/java/br/com/joaogosmani/jgprojetos/api/hateoas/CargoAssembler.java
@@ -0,0 +1,50 @@
+package br.com.joaogosmani.jgprojetos.api.hateoas;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.SimpleRepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import br.com.joaogosmani.jgprojetos.api.controllers.CargoControllerApi;
+import br.com.joaogosmani.jgprojetos.models.Cargo;
+
+@Component
+public class CargoAssembler implements SimpleRepresentationModelAssembler<Cargo>{
+
+    @Override
+    public void addLinks(EntityModel<Cargo> resource) {
+        Long id = resource.getContent().getId();
+
+        Link selfLink = linkTo(methodOn(CargoControllerApi.class).buscarPorId(id))
+            .withSelfRel()
+                .withType("GET");
+
+        Link editarLink = linkTo(methodOn(CargoControllerApi.class).atualizar(null, id))
+            .withSelfRel()
+            .withType("PUT");
+
+        Link excluirLink = linkTo(methodOn(CargoControllerApi.class).excluirPorId(id))
+            .withSelfRel()
+            .withType("DELETE");
+
+        resource.add(selfLink, editarLink, excluirLink);
+    }
+
+    @Override
+    public void addLinks(CollectionModel<EntityModel<Cargo>> resources) {
+        Link cadastroLink = linkTo(methodOn(CargoControllerApi.class).cadastrar(null))
+            .withSelfRel()
+            .withType("POST");
+
+        Link selfLink = linkTo(methodOn(CargoControllerApi.class).buscarTodos())
+            .withSelfRel()
+            .withType("GET");
+
+        resources.add(selfLink, cadastroLink);
+    }
+
+}
