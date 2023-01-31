626038c6e074d4a47f566d17005e073b673a8d60
Joao Gosmani <joaogosmani@gmail.com>
2023-01-31 13:53:37 -0300

    Adicionando os links das actions na listagem de Cargos

--- src/main/java/br/com/joaogosmani/jgprojetos/api/controllers/CargoControllerApi.java
+++ src/main/java/br/com/joaogosmani/jgprojetos/api/controllers/CargoControllerApi.java
@@ -1,11 +1,16 @@
 package br.com.joaogosmani.jgprojetos.api.controllers;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,7 +34,27 @@ public class CargoControllerApi {
 
     @GetMapping
     public List<Cargo> buscarTodos() {
-        return cargoService.buscarTodos();
+        List<Cargo> cargos = cargoService.buscarTodos();
+
+        cargos.forEach(cargo -> {
+            Long id = cargo.getId();
+
+            Link selfLink = linkTo(methodOn(CargoControllerApi.class).buscarPorId(id))
+                .withSelfRel()
+                .withType("GET");
+
+            Link editarLink = linkTo(methodOn(CargoControllerApi.class).atualizar(null, id))
+                .withSelfRel()
+                .withType("PUT");
+
+                Link excluirLink = linkTo(methodOn(CargoControllerApi.class).excluirPorId(id))
+                    .withSelfRel()
+                    .withType("DELETE");
+
+            cargo.add(selfLink, editarLink, excluirLink);
+        });
+
+        return cargos;
     }
 
     @GetMapping("/{id}")
@@ -48,10 +73,11 @@ public class CargoControllerApi {
         return cargoService.atualizar(cargoDTO, id);
     }
 
-    @DeleteMapping("/{id}")
-    @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void excluirPorId(@PathVariable Long id) {
+@DeleteMapping("/{id}")
+    public ResponseEntity<?> excluirPorId(@PathVariable Long id) {
         cargoService.excluirPorId(id);
+
+            return ResponseEntity.noContent().build();
     }
 
 }
